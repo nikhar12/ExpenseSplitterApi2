e7b2232f8f96ea63a1dff52537a0c70e2c970200
Your Name <youremail@domain.com>
2019-12-11 15:50:00 +0530

    integrate socketio bug free

--- index.js
+++ index.js
@@ -5,10 +5,26 @@ const bodyParser = require('body-parser')
 const appConfig = require('./appconfig/appconfig')
 const mongoose = require('mongoose');
 
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-const app = new express()
+server.listen(3000);
+let db = mongoose.connect(appConfig.db, { useNewUrlParser: true });
+ 
+//const app = new express()
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+
+/* 
+const server = http.createServer(app);
+ //start listening to http server
+ //console.log(appConfig);
+server.listen(appConfig.port);
+const socketLib = require("./libs/socketLib");
+const socketServer = socketLib.setServer(server); */
+
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -25,12 +41,7 @@ route.setRouter(app);
     console.log("meowing on 3000");
     let db = mongoose.connect(appConfig.db, { useNewUrlParser: true });
     console.log(db);
-}) */
+})
+ */
 
 
-const server = http.createServer(app);
- //start listening to http server
- //console.log(appConfig);
-server.listen(appConfig.port);
-const socketLib = require("./libs/socketLib");
-const socketServer = socketLib.setServer(server);
