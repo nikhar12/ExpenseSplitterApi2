8df60fac42988117245958093edd3ce6ed8700c6
nikhar12 <youremail@domain.com>
2019-12-17 01:29:16 +0530

    adwa

--- routes/route.js
+++ routes/route.js
@@ -21,7 +21,7 @@ let setRouter = (app) => {
 
     app.post(baseurl+'/group/expense',controllerem.getAllExpensesForGroup);
     app.get(baseurl+'/expense/:expenseid', controllerem.getExpense)
-    app.get(baseurl+'/expense/users', controllerem.getAllUsersForAExpense);
+    app.post(baseurl+'/expense/users', controllerem.getAllUsersForAExpense);
     //app.post(baseurl+'/expense/create')
     
     app.post(baseurl+'/group/:groupid/expense/create',controllerem.AddExpense)
